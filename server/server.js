fa7e061e9ce193dd8399142b11684413b282dc74
phamdat <phamdat@phamdats-MacBook-Pro.local>
2023-11-24 09:28:15 +0700

    allow cors

--- server/server.js
+++ server/server.js
@@ -19,6 +19,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 //Models
 
 const { User } = require("./model/user");
